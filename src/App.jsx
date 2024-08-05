9c189bad3ef0f6b7c9dfd1871950b535a17f600e
Anna <iaanya27@gmail.com>
2024-08-05 15:37:22 +0300

    added components

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+import { ContactForm } from './components/ContactForm/ContactForm'
+import { SearchBox } from './components/SearchBox/SearchBox'
+import { ContactList } from './components/ContactList/ContactList'
+// import {Contact} from './components/Contact/Contact'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+  <h1>Phonebook</h1>
+  <ContactForm />
+  <SearchBox />
+  <ContactList />
+</div>
+
+    </>
+  )
+}
+
+export default App
